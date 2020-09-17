6bdc9c2ff12b4c5e7599755b464c427fac8b95c5
prasad <prasad@debian.prasad>
2020-09-17 09:06:11 +0530

    Updates

---
+++ precache-manifest.fc71a5b3bd69d61363b9b12ba30067b5.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "e6b039600dc30b21eb32ace7498ac5bb",
+    "url": "/me/index.html"
+  },
+  {
+    "revision": "3d6f22dc6146708770ad",
+    "url": "/me/static/css/main.5f361e03.chunk.css"
+  },
+  {
+    "revision": "b97743b1076a4f90e2bf",
+    "url": "/me/static/js/2.8f18329a.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/me/static/js/2.8f18329a.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "3d6f22dc6146708770ad",
+    "url": "/me/static/js/main.e59c662a.chunk.js"
+  },
+  {
+    "revision": "580988083407601e9880",
+    "url": "/me/static/js/runtime-main.0e69a599.js"
+  }
+]);
